44b64ced39af59e1f6d5e2fbbd2ad4660d61ff10
puyolover <zych1751@naver.com>
2016-05-19 23:57:42 +0900

    hi

--- featureExtractors.py
+++ featureExtractors.py
@@ -109,4 +109,37 @@ class CustomExtractor(FeatureExtractor):
     """
     def getFeatures(self, state, action):
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        food = state.getFood()
+        walls = state.getWalls()
+        ghosts = state.getGhostPositions()
+        ghostsStates = state.getGhostStates()
+        ghostScaredTime =  state.data.agentStates[1].scaredTimer
+
+        features = util.Counter()
+
+        features["bias"] = 1.0
+
+        # compute the location of pacman after he takes the action
+        x, y = state.getPacmanPosition()
+        dx, dy = Actions.directionToVector(action)
+        next_x, next_y = int(x + dx), int(y + dy)
+
+        # count the number of ghosts 1-step away
+        features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
+
+        # if there is no danger of ghosts then add the food feature
+        if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y] and ghostScaredTime == 0:
+            features["eats-food"] = 1.0
+
+        dist = closestFood((next_x, next_y), food, walls)
+        if dist is not None:
+            # make the distance a number less than one otherwise the update
+            # will diverge wildly
+            features["closest-food"] = float(dist) / (walls.width * walls.height)
+
+        if ghostScaredTime > 0:
+            features["powerful"] = 10.0
+        features.divideAll(10.0)
+
+
+        return features
